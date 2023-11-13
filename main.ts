afd08a2ef97f558e44e37abad8cae53c93c215f4
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-13 21:14:15 +0100

    Convert factory function to a class

--- main.ts
+++ main.ts
@@ -1,14 +1,42 @@
-const Element = (type: string, attributes = {}, classes: string[] = []) =>
-({
-  src: (value: string) => Element(type, { ...attributes, src: value }, classes),
-  id: (value: string) => Element(type, { ...attributes, id: value }, classes),
-  href: (value: string) => Element(type, { ...attributes, href: value }, classes),
-  text: (value: string) => Element(type, { ...attributes, textContent: value }, classes),
-  class: (...newClasses: string[]) => Element(type, attributes, [...classes].concat(newClasses)),
-  fold: () => {
-    const elm = document.createElement(type);
-    Object.entries(attributes).forEach(([key, value]) => elm[key] = value);
-    classes.forEach(className => elm.classList.add(className));
+class Element {
+  private readonly type: string;
+  private readonly attributes: {};
+  private readonly classes: string[];
+
+  private constructor(type: string, attributes: {}, classes: string[]) {
+    this.type = type;
+    this.attributes = attributes;
+    this.classes = classes;
+  }
+
+  of(type: string) {
+    return new Element(type, {}, []);
+  }
+
+  src(value: string) {
+    return new Element(this.type, { ...this.attributes, src: value }, this.classes);
+  }
+
+  id(value: string) {
+    return new Element(this.type, { ...this.attributes, id: value }, this.classes)
+  }
+
+  href(value: string) {
+    new Element(this.type, { ...this.attributes, href: value }, this.classes)
+  }
+
+  text(value: string) {
+    new Element(this.type, { ...this.attributes, textContent: value }, this.classes)
+  }
+
+  class(...newClasses: string[]) {
+    new Element(this.type, this.attributes, [...this.classes].concat(newClasses))
+  }
+
+  fold() {
+    const elm = document.createElement(this.type);
+    Object.entries(this.attributes).forEach(([key, value]) => elm[key] = value);
+    this.classes.forEach(className => elm.classList.add(className));
     return elm;
   }
-})
+}
