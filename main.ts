047229f177ed1945622b640d188b85366883603c
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-13 21:04:07 +0100

    Create element factory function

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+const Element = (type: string, attributes = {}, classes: string[] = []) =>
+({
+  src: (value: string) => Element(type, { ...attributes, src: value }, classes),
+  id: (value: string) => Element(type, { ...attributes, id: value }, classes),
+  href: (value: string) => Element(type, { ...attributes, href: value }, classes),
+  text: (value: string) => Element(type, { ...attributes, textContent: value }, classes),
+  class: (...newClasses: string[]) => Element(type, attributes, [...classes].concat(newClasses)),
+  fold: () => {
+    const elm = document.createElement(type);
+    Object.entries(attributes).forEach(([key, value]) => elm[key] = value);
+    classes.forEach(className => elm.classList.add(className));
+    return elm;
+  }
+})
